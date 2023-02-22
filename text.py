8c646a964d26188d5061cf4796d7653a052c47a6
Afrosjar <123934313+Afrosjar@users.noreply.github.com>
2023-02-22 14:34:44 +0300

    Update text.py
    Изменение вывода ответа в соответствии с задачами

--- text.py
+++ text.py
@@ -16,7 +16,7 @@ c = s.readlines()
 d = open ('Final_text.txt','w', encoding = 'utf-8')
 # Заходим на проверку по первому IF 
 if len(a) < len(b) and len(a) < len(c):
-    d.writelines(f"'text1.txt'\n{len(a)}\n")
+    d.writelines(f"text1.txt\n{len(a)}\n")
 
     with open ('Final_text.txt','a',encoding = 'utf-8') as f:
         counter = 0
@@ -27,26 +27,26 @@ if len(a) < len(b) and len(a) < len(c):
             
     if len(b) < len(c):
         d = open ('Final_text.txt','a', encoding = 'utf-8')
-        d.writelines(f"'text2.txt'\n{len(b)}\n")
+        d.writelines(f"text2.txt\n{len(b)}\n")
         counter = 0
         for id, line in enumerate(b,start = 1):
             if counter <= id:
                 d.write(f'{line}')
         d.write(f'\n')
-        d.writelines(f"'text3.txt'\n{len(c)}\n")
+        d.writelines(f"text3.txt\n{len(c)}\n")
         counter1 = 0
         for id, line in enumerate(c,start = 1):
             if counter1 <= id:
                 d.write(f'{line}')
     if len(c) < len(b):
         d = open ('Final_text.txt','a', encoding = 'utf-8')
-        d.writelines(f"'text2.txt'\n{len(c)}\n")
+        d.writelines(f"text2.txt\n{len(c)}\n")
         counter = 0
         for id, line in enumerate(c,start = 1):
             if counter <= id:
                 d.write(f'{line}')
         d.write(f'\n')
-        d.writelines(f"'text3.txt'\n{len(b)}\n")
+        d.writelines(f"text3.txt\n{len(b)}\n")
         counter1 = 0
         for id, line in enumerate(b,start = 1):
             if counter1 <= id:
@@ -54,7 +54,7 @@ if len(a) < len(b) and len(a) < len(c):
     
  # Заходим на проверку по второму IF        
 if len(b) < len(a) and len(b) < len(c):
-    d.writelines(f"'text2.txt'\n{len(b)}\n")
+    d.writelines(f"text2.txt\n{len(b)}\n")
 
     with open ('Final_text.txt','a',encoding = 'utf-8') as f:
         counter = 0
@@ -65,33 +65,33 @@ if len(b) < len(a) and len(b) < len(c):
             
     if len(a) < len(c):
         d = open ('Final_text.txt','a', encoding = 'utf-8')
-        d.writelines(f"'text1.txt'\n{len(a)}\n")
+        d.writelines(f"text1.txt\n{len(a)}\n")
         counter = 0
         for id, line in enumerate(a,start = 1):
             if counter <= id:
                 d.write(f'{line}')
         d.write(f'\n')
-        d.writelines(f"'text3.txt'\n{len(c)}\n")
+        d.writelines(f"text3.txt\n{len(c)}\n")
         counter1 = 0
         for id, line in enumerate(c,start = 1):
             if counter1 <= id:
                 d.write(f'{line}')
     if len(c) < len(a):
         d = open ('Final_text.txt','a', encoding = 'utf-8')
-        d.writelines(f"'text3.txt'\n{len(c)}\n")
+        d.writelines(f"text3.txt\n{len(c)}\n")
         counter = 0
         for id, line in enumerate(c,start = 1):
             if counter <= id:
                 d.write(f'{line}')
         d.write(f'\n')
-        d.writelines(f"'text1.txt'\n{len(a)}\n")
+        d.writelines(f"text1.txt\n{len(a)}\n")
         counter1 = 0
         for id, line in enumerate(a,start = 1):
             if counter1 <= id:
                 d.write(f'{line}')
 # Заходим на проверку по последнему IF
 if len(c) < len(a) and len(c) < len(b):
-    d.writelines(f"'text3.txt'\n{len(c)}\n")
+    d.writelines(f"text3.txt\n{len(c)}\n")
 
     with open ('Final_text.txt','a',encoding = 'utf-8') as f:
         counter = 0
@@ -102,26 +102,26 @@ if len(c) < len(a) and len(c) < len(b):
             
     if len(b) < len(a):
         d = open ('Final_text.txt','a', encoding = 'utf-8')
-        d.writelines(f"'text2.txt'\n{len(b)}\n")
+        d.writelines(f"text2.txt\n{len(b)}\n")
         counter = 0
         for id, line in enumerate(b,start = 1):
             if counter <= id:
                 d.write(f'{line}')
         d.write(f'\n')
-        d.writelines(f"'text1.txt'\n{len(a)}\n")
+        d.writelines(f"text1.txt\n{len(a)}\n")
         counter1 = 0
         for id, line in enumerate(a,start = 1):
             if counter1 <= id:
                 d.write(f'{line}')
     if len(a) < len(b):
         d = open ('Final_text.txt','a', encoding = 'utf-8')
-        d.writelines(f"'text1.txt'\n{len(a)}\n")
+        d.writelines(f"text1.txt\n{len(a)}\n")
         counter = 0
         for id, line in enumerate(a,start = 1):
             if counter <= id:
                 d.write(f'{line}')
         d.write(f'\n')
-        d.writelines(f"'text2.txt'\n{len(b)}\n")
+        d.writelines(f"text2.txt\n{len(b)}\n")
         counter1 = 0
         for id, line in enumerate(b,start = 1):
             if counter1 <= id:
@@ -155,26 +155,3 @@ s.close()
 
 
 
-# Необходимо объединить их в один по следующим правилам:
-
-# Содержимое исходных файлов в результирующем файле должно быть отсортировано по количеству строк в них (то есть первым нужно записать файл с наименьшим количеством строк, а последним - с наибольшим)
-# Содержимое файла должно предваряться служебной информацией на 2-х строках: имя файла и количество строк в нем
-# Пример Даны файлы: 1.txt
-
-# Строка номер 1 файла номер 1
-# Строка номер 2 файла номер 1
-# 2.txt
-
-# Строка номер 1 файла номер 2
-# Итоговый файл:
-
-# 2.txt
-# 1
-# Строка номер 1 файла номер 2
-# 1.txt
-# 8
-# Строка номер 1 файла номер 1
-# Строка номер 2 файла номер 1
-# 3.txt
-# 9
-# строки 1-9 файла номер 3 по очереди
